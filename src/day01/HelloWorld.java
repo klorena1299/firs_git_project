8d293dceb8089e897a6c5080c8dde0b93cf1b67d
klorena1299 <kdon.lorena@gmail.com>
2025-06-16 12:59:46 -0400

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
         System.out.println("Hello World");
         System.out.println("Hello Cydeo");
-        System.out.println("Hello Programmers");
+        System.out.println("Hello Programmers"):
         
 
     }
